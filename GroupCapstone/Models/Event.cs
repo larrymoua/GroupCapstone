deb94bd129bf9dcefe9b2fb88059f3e597bfd3c0
FOUNG <larrymoua24@yahoo.com>
2019-03-13 12:59:30 -0500

    Added attributes, Rating , ticketspurchased, ticketsAvail, TicketPrice, fk Category

--- GroupCapstone/Models/Event.cs
+++ GroupCapstone/Models/Event.cs
@@ -20,11 +20,70 @@ namespace GroupCapstone.Models
         public DateTime EventDate{ get; set; }
         public string Street { get; set; }
         public string City { get; set; }
-        public string State { get; set; }
+        public States State { get; set; }
         public int Zip { get; set; }
-
+        public int? Rating { get; set; }  
+        public int TicketsAvailable { get; set; }
+        public Decimal TicketPrice { get; set; }
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public virtual Category categories { get; set; }
         public int HolderId { get; set; }
         [ForeignKey("HolderId")]
         public virtual EventHolder EventHolders { get; set; }
+    }
+    public enum States
+    {
+        Alabama,
+        Alaska,
+        Arizona,
+        Arkansas,
+        California,
+        Colorado,
+        Connecticut,
+        Delaware,
+        Florida,
+        Georgia,
+        Hawaii,
+        Idaho,
+        Illinois,
+        Indiana,
+        Iowa,
+        Kansas,
+        Kentucky,
+        Louisiana,
+        Maine,
+        Maryland,
+        Massachusetts,
+        Michigan,
+        Minnesota,
+        Mississippi,
+        Missouri,
+        Montana ,
+        Nebraska,
+        Nevada,
+        NewHampshire,
+        NewJersey,
+        NewMexico,
+        NewYork,
+        NorthCarolina,
+        NorthDakota,
+        Ohio,
+        Oklahoma,
+        Oregon,
+        Pennsylvania ,
+        RhodeIsland,
+        SouthCarolina,
+        SouthDakota,
+        Tennessee,
+        Texas,
+        Utah,
+        Vermont,
+        Virginia,
+        Washington,
+        WestVirginia,
+        Wisconsin,
+        Wyoming
+
     }
 }
--- GroupCapstone/Models/EventHolder.cs
+++ GroupCapstone/Models/EventHolder.cs
@@ -17,8 +17,9 @@ namespace GroupCapstone.Models
         public string LastName { get; set; }
         [Display(Name = "Company's Name")]
         public string CompanyName { get; set; }
+        public int AvgRating { get; set; }
 
-        [ForeignKey("ApplicationUser")]
+        [ForeignKeyAttribute("ApplicationUser")]
         public string ApplicationUserId { get; set; }
         public ApplicationUser ApplicationUser { get; set; }
     }
