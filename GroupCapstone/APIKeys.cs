a7f1d96d73ea9232dde8120bb8d2da7a58803c86
Fp38SilSha <faperez38s@gmail.com>
2019-03-13 14:53:51 -0500

    Api keys static file created. Loading up keys as soon as we are sure it is hidden.

---
+++ GroupCapstone/APIKeys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GroupCapstone
+{
+    public static class APIKeys
+    {
+        const string stripeKey = "api";
+        const string googleMapKey = "api";
+        const string openWeatherKey = "api";
+
+    }
+}
