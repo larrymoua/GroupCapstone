a197ff4572f43720ccbb28447a55b35a470fa05a
Luke Baierl <lukejb1984@hotmail.com>
2019-03-14 14:03:41 -0500

    Initial commit.

---
+++ GroupCapstone/Controllers/EventController.cs
@@ -0,0 +1,95 @@
+﻿using GroupCapstone.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GroupCapstone.Controllers
+{
+    public class EventController : Controller
+    {
+        // GET: Event
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Event/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Event/Create
+        public ActionResult Create()
+        {
+            Comments comments = new Comments();
+            return View(comments);
+        }
+
+        // POST: Event/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "UserName,Comment,Date")]Comments comments)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                if (ModelState.IsValid)
+                {
+                    db.events.Add(Comments);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Event/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Event/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Event/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Event/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ GroupCapstone/Models/Comments.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GroupCapstone.Models
+{
+    public class Comments
+    {
+        [Key]
+
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Add Comment")]
+        public string Comment { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
