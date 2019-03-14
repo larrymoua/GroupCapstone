049a41d29c5cfcc848bd88ebe1aea5cbc55f3deb
FOUNG <larrymoua24@yahoo.com>
2019-03-14 16:13:39 -0500

    Google map api shows on events details. Dropdown list shows for category when creating and editing events

--- GroupCapstone/Migrations/201903141936361_update.cs
+++ GroupCapstone/Migrations/201903141936361_update.cs
@@ -7,14 +7,10 @@ namespace GroupCapstone.Migrations
     {
         public override void Up()
         {
-            AddColumn("dbo.Events", "Category", c => c.Int(nullable: false));
-            DropColumn("dbo.Events", "Cat");
         }
         
         public override void Down()
         {
-            AddColumn("dbo.Events", "Cat", c => c.Int(nullable: false));
-            DropColumn("dbo.Events", "Category");
         }
     }
 }
