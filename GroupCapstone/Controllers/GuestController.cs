30d299803e165eeeedd11714bf1b0cf94cc647f0
FOUNG <larrymoua24@yahoo.com>
2019-03-18 17:32:00 -0500

    readd eventsthisweeek logic in guesthome

--- GroupCapstone/Controllers/GuestController.cs
+++ GroupCapstone/Controllers/GuestController.cs
@@ -30,7 +30,17 @@ namespace GroupCapstone.Controllers
             var currentDate = DateTime.Now;
             int currentWeek = GetWeekNumber(currentDate);
             var eventsInZip = db.events.Where(e => e.Zip == currentGuest.Zip).ToList();
-          var typeList = Enum.GetValues(typeof(Category))
+            List<Models.Event> eventsThisWeek = new List<Models.Event> { };
+
+            foreach (var foundEvent in eventsInZip)
+            {
+                int eventWeek = GetWeekNumber(foundEvent.EventDate);
+                if (eventWeek == currentWeek)
+                {
+                    eventsThisWeek.Add(foundEvent);
+                }
+            }
+            var typeList = Enum.GetValues(typeof(Category))
           .Cast<Category>()
           .Select(t => new AcessClass
           {
@@ -38,7 +48,7 @@ namespace GroupCapstone.Controllers
 
           });
                 ViewBag.ListData = typeList;
-            return View(eventsInZip);
+            return View(eventsThisWeek);
         }
         public ActionResult Filter(string id)
         {
