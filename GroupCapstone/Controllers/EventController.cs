0f75ec05835dbaa325d3e7b39bf81d51caea8a0f
Fp38SilSha <faperez38s@gmail.com>
2019-03-19 18:27:57 -0500

    Got rid of useless code in the event controller.

--- GroupCapstone/Controllers/EventController.cs
+++ GroupCapstone/Controllers/EventController.cs
@@ -16,22 +16,7 @@ namespace GroupCapstone.Controllers
         {
             _context = new ApplicationDbContext();
         }
-        // GET: Event
 
-        public ActionResult Index()
-        {
-            
-            return View();
-        }
-
-        // GET: Event/Details/5
-        public ActionResult Details(int id)
-        {
-            
-            return View();
-        }
-
-        // GET: Event/Create
         [HttpGet]
         public ActionResult AddComment(int id)
         {
@@ -56,85 +41,5 @@ namespace GroupCapstone.Controllers
             return RedirectToAction("Details");
         }
 
-        // POST: Event/Create
-        [HttpPost]
-        public ActionResult Create()
-        {
-            //if/*(events.EventId == 0)*/
-            //{
-            //    /*_context.events.Add(events);*/
-            //}
-            //else
-            //{
-            //    // TODO: Add insert logic here
-            //    /*var eventInDB = _context.events.Single(e => e.EventId == events.EventId);*/
-
-            //    /*eventInDB.CommentId = events.CommentId;*/
-            //}
-            _context.SaveChanges();
-            return RedirectToAction("Index", "Events");
-        }
-
-        // GET: Event/Edit/5
-        [HttpGet]
-        public ActionResult Edit(int id)
-        {
-            /*var events = _context.events.SingleOrDefault(e => e.EventId == id);
-            events.Comment = _context.Comments.ToList();
-            if (events == null)
-            {
-                return HttpNotFound();
-            }*/
-
-            return View(/*events*/);
-        }
-        
-        [HttpGet]
-        public ActionResult Edit(Event events)
-        {
-            /*var eventInDB = _context.events.Single(e => e.EventId == events.EventId);
-            eventInDB.CommentId = events.CommentId;
-            eventInDB.Comment = _context.Comments.ToList();
-            _context.SaveChanges();*/
-            return RedirectToAction("Index", "Events");
-        }
-
-        // POST: Event/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Event/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Event/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
