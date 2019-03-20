d9d88a953427513f056d510e906dca1a0466450f
Ben Pantazon <pantazon-b@live.com>
2019-03-20 07:49:13 -0500

    Edited the view details view for Events filtered by rating

--- GroupCapstone/Controllers/HomeController.cs
+++ GroupCapstone/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GroupCapstone.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +10,26 @@ namespace GroupCapstone.Controllers
 {
     public class HomeController : Controller
     {
+        ApplicationDbContext context;
+        public HomeController()
+        {
+            context = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
+            var currentUser = User.Identity.GetUserId();
+            if (User.IsInRole("EventHolder"))
+            {
+                var eventHolder = context.eventHolders.Where(e => e.ApplicationUserId == currentUser).SingleOrDefault();
+                return RedirectToAction("MyEvents", "EventHolder");
+            }
+            else if (User.IsInRole("Guest"))
+            {
+                var foundGuest = context.guests.Where(e => e.ApplicationUserId == currentUser).SingleOrDefault();
+                var checkGuest = context.guests.Where(c => c.GuestId == foundGuest.GuestId);
+                return RedirectToAction("GuestHome", "Guest");
+            }
             return View();
         }
 
